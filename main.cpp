a0655a6a239f93796322c8b47edf35a5b756c2fb
shubhamparekar <49017702+shubhamparekar@users.noreply.github.com>
2025-01-25 16:06:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
-// main.cpp
-
 #include "tic_tac_toe.hpp"
 #include <iostream>
+#include <windows.h>
+
+void MaximizeConsoleWindows()
+{
+    HWND consoleWindow = GetConsoleWindow();
+    ShowWindow(consoleWindow, SW_MAXIMIZE);
+}
+
+void SetConsoleFontSize(int fontSize)
+{
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0; // Width of each character in the font
+    cfi.dwFontSize.Y = fontSize; // Height
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_NORMAL;
+    wcscpy_s(cfi.FaceName, L"Consolas"); // Choose your font
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+}
 
 int main() {
+
+    MaximizeConsoleWindows();
+    SetConsoleFontSize(30);
+
     TicTacToe game;
     game.StartGame();
 
